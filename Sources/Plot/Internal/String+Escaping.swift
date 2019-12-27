61ed60a516ef7017f7d47781b3e335d86aa32419
John Sundell <john@sundell.co>
2019-12-27 21:38:34 +0000

    Node: Prevent HTML entities from being double-escaped (#11)
    This patch makes sure that any HTML entities contained within a string passed
    to `Node.text` won’t be double-escaped, by making the string escaping code
    read ahead when encountering an ampersand, and then filling in any pending
    text following it once it has determined that it wasn’t the start of an
    HTML entity. With this approach, the performance impact on the escaping code
    is very low, since it retains close to `O(N)` time complexity.

--- Sources/Plot/Internal/String+Escaping.swift
+++ Sources/Plot/Internal/String+Escaping.swift
@@ -6,17 +6,44 @@
 
 internal extension String {
     func escaped() -> String {
-        String(flatMap { character -> String in
+        var pendingAmpersandString: String?
+
+        func flushPendingAmpersandString(
+            withSuffix suffix: String? = nil,
+            resettingTo newValue: String? = nil
+        ) -> String {
+            let pending = pendingAmpersandString
+            pendingAmpersandString = newValue
+            return pending.map { "&amp;\($0)\(suffix ?? "")" } ?? suffix ?? ""
+        }
+
+        return String(flatMap { character -> String in
             switch character {
             case "<":
-                return "&lt;"
+                return flushPendingAmpersandString(withSuffix: "&lt;")
             case ">":
-                return "&gt;"
+                return flushPendingAmpersandString(withSuffix: "&gt;")
             case "&":
-                return "&amp;"
+                return flushPendingAmpersandString(resettingTo: "")
+            case ";":
+                let pending = pendingAmpersandString.map { "&\($0);" }
+                pendingAmpersandString = nil
+                return pending ?? ";"
+            case "#" where pendingAmpersandString?.isEmpty == true:
+                pendingAmpersandString = "#"
+                return ""
             default:
+                if let pending = pendingAmpersandString {
+                    guard character.isLetter || character.isNumber else {
+                        return flushPendingAmpersandString(withSuffix: String(character))
+                    }
+
+                    pendingAmpersandString = "\(pending)\(character)"
+                    return ""
+                }
+
                 return "\(character)"
             }
-        })
+        }) + flushPendingAmpersandString()
     }
 }
