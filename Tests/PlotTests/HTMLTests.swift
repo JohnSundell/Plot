d408b067eaf21b0fc4906ddeb9386bef9b1f3d8d
John Sundell <john@sundell.co>
2020-01-29 22:12:40 +0100

    HTML script tests: Minor fixes

--- Tests/PlotTests/HTMLTests.swift
+++ Tests/PlotTests/HTMLTests.swift
@@ -483,15 +483,17 @@ final class HTMLTests: XCTestCase {
     func testJavaScript() {
         let html = HTML(
             .head(
-                .script(.src("file.js")),
-                .script(.async(), .src("file.js")),
-                .script(.defer(), .src("file.js"))
+                .script(.src("script.js")),
+                .script(.async(), .src("async.js")),
+                .script(.defer(), .src("deferred.js"))
             ),
             .body(.script(#"console.log("Consider going JS-free :)")"#))
         )
 
         assertEqualHTMLContent(html, """
-        <head><script src="file.js"></script><script async src="file.js"></script><script defer src="file.js"></script></head>\
+        <head><script src="script.js"></script>\
+        <script async src="async.js"></script>\
+        <script defer src="deferred.js"></script></head>\
         <body><script>console.log("Consider going JS-free :)")</script></body>
         """)
     }
