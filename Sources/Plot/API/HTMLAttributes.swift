cf2a2d00fdf963a48ff076be6d2a43ede7bc2bb4
John Sundell <john@sundell.co>
2020-01-29 22:10:44 +0100

    Script attributes: Minor documentation and organization fixes

--- Sources/Plot/API/HTMLAttributes.swift
+++ Sources/Plot/API/HTMLAttributes.swift
@@ -416,6 +416,20 @@ public extension Node where Context: HTMLIntegrityContext {
     }
 }
 
+// MARK: - Scripts
+
+public extension Node where Context == HTML.ScriptContext {
+    /// Assign that the element's script should be loaded in `async` mode.
+    static func async() -> Node {
+        .attribute(named: "async", value: nil, ignoreIfValueIsEmpty: false)
+    }
+    
+    /// Assign that the element's script should be loaded in `defer` mode.
+    static func `defer`() -> Node {
+        .attribute(named: "defer", value: nil, ignoreIfValueIsEmpty: false)
+    }
+}
+
 // MARK: - Other, element-specific attributes
 
 public extension Node where Context == HTML.AbbreviationContext {
@@ -423,19 +437,5 @@ public extension Node where Context == HTML.AbbreviationContext {
     /// - parameter title: The title to assign.
     static func title(_ title: String) -> Node {
         .attribute(named: "title", value: title)
-    }
-}
-
-// MARK: - Script
-
-public extension Node where Context == HTML.ScriptContext {
-    /// Assign the element should load script in `async` mode.
-    static func async() -> Node {
-        .attribute(named: "async", value: nil, ignoreIfValueIsEmpty: false)
-    }
-    
-    /// Assign the element should load script in `defer` mode.
-    static func `defer`() -> Node {
-        .attribute(named: "defer", value: nil, ignoreIfValueIsEmpty: false)
     }
 }
