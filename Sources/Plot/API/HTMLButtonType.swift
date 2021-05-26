c143ed7731fdb658ef3632cc5d20830af7f206db
Dave Verwer <dave.verwer@gmail.com>
2021-05-26 11:35:15 +0100

    Added `HTMLButtonType`.

---
+++ Sources/Plot/API/HTMLButtonType.swift
@@ -0,0 +1,15 @@
+/**
+*  Plot
+*  Copyright (c) John Sundell 2019
+*  MIT license, see LICENSE file for details
+*/
+
+import Foundation
+
+/// Enum that defines various button types that can be used with the
+/// `<button/>` HTML element. For example `.button(.type(.submit))`.
+public enum HTMLButtonType: String {
+    case button
+    case reset
+    case submit
+}
